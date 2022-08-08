39a4f1f02935dfd9936cdefd9966ecdb85f8b753
maryamhaitham <marym.ihnj@gmail.com>
2022-08-08 14:24:32 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Your code here
+
+  const dodger = document.getElementById('dodger');
+
+// Move Dodger left
+
+document.addEventListener('keydown', function(e) {
+  if (e.key === "ArrowLeft") {
+    moveDodgerLeft()
+  }
+})
+
+function moveDodgerLeft() {
+  const leftNumbers = dodger.style.left.replace('px', '');
+  const left = parseInt(leftNumbers, 10)
+
+    dodger.style.left = `${left - 1}px`
+  
+}
+
+// Move Dodger Right
+
+document.addEventListener('keydown', function(e) {
+  if (e.key === "ArrowRight") {
+    moveDodgerRight()
+  }
+})
+
+function moveDodgerRight() {
+  const leftNumbers = dodger.style.left.replace('px', '');
+  const left = parseInt(leftNumbers, 10)
+  
+    dodger.style.left = `${left + 1}px`
+}
+
+
